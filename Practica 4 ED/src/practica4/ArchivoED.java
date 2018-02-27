da48a65ebdd54bf6c9884533cda0e31e05e4f0b4
Studium <Studium@192.168.1.130>
2018-02-27 10:12:55 +0100

    He hecho un commit para la practica 4 de Entornos de desarrollo.

---
+++ Practica 4 ED/src/practica4/ArchivoED.java
@@ -0,0 +1,10 @@
+package practica4;
+
+public class ArchivoED {
+
+	public static void main(String[] args)
+	{
+		System.out.println("Número aleatorio: " + Math.random());
+		System.out.println("Otro número aleatorio: " + Math.random());
+	}
+}
