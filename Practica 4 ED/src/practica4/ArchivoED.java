6a04128abe3fccc74aba3ac6f3110ab690565f26
Felix <felix.delrio1.@gmail.com>
2018-02-27 11:37:10 +0100

    Commit2

--- Practica 4 ED/src/practica4/ArchivoED.java
+++ Practica 4 ED/src/practica4/ArchivoED.java
@@ -4,6 +4,7 @@ public class ArchivoED {
 
 	public static void main(String[] args)
 	{
+		//Hola
 		System.out.println("Número aleatorio: " + Math.random());
 		System.out.println("Otro número aleatorio: " + Math.random());
 	}
