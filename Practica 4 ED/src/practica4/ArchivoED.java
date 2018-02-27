79f4c302a712a14e2a1297ef3ef5d6c4c7f943ba
rgarridolora40 <35599539+rgarridolora40@users.noreply.github.com>
2018-02-27 11:47:53 +0100

    Update ArchivoED.java
    He añadido un comentario a la clase de Félix.

--- Practica 4 ED/src/practica4/ArchivoED.java
+++ Practica 4 ED/src/practica4/ArchivoED.java
@@ -1,11 +1,12 @@
 package practica4;
 
 public class ArchivoED {
+	//comentario de Rafael
 
 	public static void main(String[] args)
 	{
 		//Hola
-		System.out.println("Número aleatorio: " + Math.random());
-		System.out.println("Otro número aleatorio: " + Math.random());
+		System.out.println("NÃºmero aleatorio: " + Math.random());
+		System.out.println("Otro nÃºmero aleatorio: " + Math.random());
 	}
 }
